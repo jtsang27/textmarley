9ea6b1967a85d6ebef6e6682b8aa6c92849344ad
evanliu23 <eliu23@bu.edu>
2025-02-01 21:14:49 -0500

    Update

--- server/src/app.py
+++ server/src/app.py
@@ -393,4 +393,4 @@ reminder_thread = Thread(target=send_reminders, daemon=True)
 reminder_thread.start()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
