dd939c135add94d657a4c5c77664b753ba21f69c
evanliu23 <eliu23@bu.edu>
2025-02-09 19:40:28 -0500

    Solve incorrect extraction of api key

--- server/src/app.py
+++ server/src/app.py
@@ -17,7 +17,7 @@ TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 
 # OpenAI api key
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Clients
 Tclient = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
